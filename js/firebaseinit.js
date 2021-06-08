1ef996ad06b09b2de5e29c37626a85d06f994f49
Nency Shobhashana <shobhashananency@gmail.com>
2021-06-08 12:49:04 -0400

    Firebase setup

---
+++ js/firebaseinit.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyAgDjNtnKMfinuNAHddBe8OnSBeivNnUFw",
+  authDomain: "fresh-basket-1ee04.firebaseapp.com",
+  projectId: "fresh-basket-1ee04",
+  storageBucket: "fresh-basket-1ee04.appspot.com",
+  messagingSenderId: "39663268992",
+  appId: "1:39663268992:web:423982874875d703be2265",
+  measurementId: "G-0V9W5N7DX2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
